66f5b4bb3e851de0e203833a51e45ad7c19ef84e
Damian Jarek <damian.jarek93@gmail.com>
2016-03-11 15:10:22 +0100

    Added the ProbeSubVolumes prototype
    ProbeSubVolumes is used to retrieve from the kernel a
    list of all subvolumes present on a btrfs volume.

---
+++ slave/osinterface/btrfs_subvol_interface.go
@@ -0,0 +1,11 @@
+package osinterface
+
+import "github.com/djarek/btrfs-volume-manager/common/dtos"
+
+/*ProbeSubvolumes probes the kernel and retrieves all subvolumes present
+in a btrfs volume.
+*/
+func ProbeSubvolumes(volume dtos.BtrfsVolume) (subvols []dtos.BtrfsSubVolume, err error) {
+
+	return
+}
