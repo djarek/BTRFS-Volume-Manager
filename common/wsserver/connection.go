37ff1ef06457856f96c835885caf159914f80773
Damian Jarek <damian.jarek93@gmail.com>
2016-03-17 00:52:16 +0100

    Refactor wsserver
    Removed the authenticated variable from the Connection struct.
    Redesigned the connection closing flow, so that closing code is
    only called by the writer goroutine (internalClose() method).

--- common/wsserver/connection.go
+++ common/wsserver/connection.go
@@ -1,7 +1,9 @@
 package wsserver
 
 import (
+	"errors"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/djarek/btrfs-volume-manager/common/dtos"
@@ -13,6 +15,7 @@ const (
 	pingInterval              = (pongTimeout * 9) / 10
 	writeTimeout              = 10 * time.Second
 	authenticationReadTimeout = writeTimeout
+	writeChannelSize          = 16
 )
 
 //SendCallback is the signature of the function that is called when an async
@@ -26,37 +29,32 @@ type RecvMessageParser interface {
 }
 
 type sendTask struct {
-	callback SendCallback
-	payload  []byte
+	callback    SendCallback
+	payload     []byte
+	messageType int
 }
 
 //Connection wraps the websocket connection
 type Connection struct {
 	wsConnection    *websocket.Conn
-	authenticated   bool
 	marshaller      dtos.WebSocketMessageMarshaller
 	parser          RecvMessageParser
 	onCloseCallback func()
-	pingTicker      *time.Ticker
 
 	writeChannel chan sendTask
+	closeOnce    sync.Once
 }
 
 func newConnection(wsConnection *websocket.Conn, marshaller dtos.WebSocketMessageMarshaller, parser RecvMessageParser) *Connection {
 	return &Connection{
-		wsConnection:  wsConnection,
-		authenticated: false,
-		marshaller:    marshaller,
-		parser:        parser,
+		wsConnection: wsConnection,
+		marshaller:   marshaller,
+		parser:       parser,
 
-		writeChannel: make(chan sendTask)}
+		writeChannel: make(chan sendTask, writeChannelSize)}
 }
 
-func (c *Connection) registerOnCloseCallback(cb func()) {
-	c.onCloseCallback = cb
-}
-
-//Authenticate performs authentication of the connection using the supplied authenticator.
+//authenticate performs authentication of the connection using the supplied authenticator.
 //It returns nil when the authentication is successful and the connection is ready to be
 //served.
 func (c *Connection) authenticate(authenticator WebSocketAuthenticator) (err error) {
@@ -72,8 +70,6 @@ func (c *Connection) authenticate(authenticator WebSocketAuthenticator) (err err
 	if err != nil {
 		log.Println("Error when authenticating: " + err.Error())
 	}
-
-	c.authenticated = true
 	return
 }
 
@@ -82,41 +78,8 @@ func (c *Connection) authenticate(authenticator WebSocketAuthenticator) (err err
 //Blocks until the reader loop exits, at which point the connection is properly
 //closed.
 func (c *Connection) Serve() {
-	if !c.authenticated {
-		log.Panicln("Serve called without valid authentication")
-	}
-
 	go c.writerLoop()
 	c.readerLoop()
-}
-
-func (c *Connection) writerLoop() {
-	c.pingTicker = time.NewTicker(pingInterval)
-	defer c.Close()
-
-	for {
-		select {
-		case <-c.pingTicker.C:
-			err := c.internalWrite(websocket.PingMessage, []byte{})
-			if err != nil {
-				log.Println("Error when sending websocket ping: " + err.Error())
-				return
-			}
-
-		case task := <-c.writeChannel:
-			err := c.internalWrite(websocket.TextMessage, task.payload)
-			task.callback(err)
-		}
-	}
-}
-
-func (c *Connection) internalWrite(msgType int, payload []byte) (err error) {
-	err = c.wsConnection.SetWriteDeadline(time.Now().Add(writeTimeout))
-	if err != nil {
-		return
-	}
-	err = c.wsConnection.WriteMessage(msgType, payload)
-	return
 }
 
 func (c *Connection) readerLoop() {
@@ -129,7 +92,8 @@ func (c *Connection) readerLoop() {
 	for {
 		_, msgBytes, err := c.wsConnection.ReadMessage()
 		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseNormalClosure) {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseNormalClosure) &&
+				websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
 				log.Println("Error when reading websocket message: " + err.Error())
 			}
 			return
@@ -144,28 +108,102 @@ func (c *Connection) readerLoop() {
 	}
 }
 
-//Close closes the underlying connection and does the necessary cleanup
-//like removing the connection from the connection manager
-func (c *Connection) Close() error {
-	if c.pingTicker != nil {
-		c.pingTicker.Stop()
-	}
-	if c.onCloseCallback != nil {
-		c.onCloseCallback()
-	}
-	return c.wsConnection.Close()
+/*Close attempts to send a proper close to the client. If the connection
+is in an invalid state, this will fail, however, all the necessary cleanup
+will be performed properly anyway.*/
+func (c *Connection) Close() {
+	c.closeOnce.Do(func() {
+		c.addSendTask(sendTask{
+			callback:    func(error) {},
+			payload:     websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""),
+			messageType: websocket.CloseMessage,
+		})
+	})
+}
+
+func (c *Connection) addSendTask(task sendTask) (err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			err = errors.New("Connection closed")
+			task.callback(err)
+		}
+	}()
+	c.writeChannel <- task
+	return
 }
 
 //SendAsync sends a WebSocketMessage asynchronously and returns immediately if an
 //error is encountered or the message write has been enqueued. If an error is
 //encountered during the network transfer, the error is passed to the callback
 func (c *Connection) SendAsync(msg dtos.WebSocketMessage, callback SendCallback) error {
-	webSocketMsgBytes, err := c.marshaller.Marshall(msg)
+	msgBytes, err := c.marshaller.Marshall(msg)
 	if err != nil {
 		log.Println("Error when marshalling WebSocketMessage: " + err.Error())
 		return err
 	}
 
-	c.writeChannel <- sendTask{callback, webSocketMsgBytes}
-	return nil
+	return c.addSendTask(sendTask{
+		callback:    callback,
+		payload:     msgBytes,
+		messageType: websocket.TextMessage,
+	})
+}
+
+func (c *Connection) writerLoop() {
+	pingTicker := time.NewTicker(pingInterval)
+	defer pingTicker.Stop()
+	defer c.internalClose()
+
+	for {
+		select {
+		case <-pingTicker.C:
+			err := c.internalWrite(websocket.PingMessage, []byte{})
+			if err != nil {
+				log.Println("Error when sending websocket ping: " + err.Error())
+				return
+			}
+
+		case task := <-c.writeChannel:
+			err := c.sendTask(task)
+			if err != nil || task.messageType == websocket.CloseMessage {
+				return
+			}
+		}
+	}
+}
+
+/*!!!!!! !!!!!!  !!!!!! DANGER !!!!!! !!!!!! !!!!!!
+The methods below are only supposed to be called by the writer goroutine
+*/
+
+func (c *Connection) flushRemainingTasks() {
+	for task := range c.writeChannel {
+		/*We ignore the error because we want to make sure the task owners are
+		notified about completion.*/
+		_ = c.sendTask(task)
+	}
+}
+
+func (c *Connection) internalClose() {
+	close(c.writeChannel)
+	c.flushRemainingTasks()
+
+	if c.onCloseCallback != nil {
+		c.onCloseCallback()
+	}
+	c.wsConnection.Close()
+}
+
+func (c *Connection) sendTask(task sendTask) error {
+	err := c.internalWrite(websocket.TextMessage, task.payload)
+	task.callback(err)
+	return err
+}
+
+func (c *Connection) internalWrite(msgType int, payload []byte) error {
+	err := c.wsConnection.SetWriteDeadline(time.Now().Add(writeTimeout))
+	if err != nil {
+		return err
+	}
+	return c.wsConnection.WriteMessage(msgType, payload)
 }
