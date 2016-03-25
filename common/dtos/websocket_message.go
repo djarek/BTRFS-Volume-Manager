3d621a372f01284ee231ca8601fa74816e765804
Damian Jarek <damian.jarek93@gmail.com>
2016-03-25 22:14:45 +0100

    Redesigned websocket message marshalling/unmarshalling
    WebSocketMessages will now store the payload as interface{}
    and be automatically marshalled and unmarshalled. This requires
    prior registering of the necessary types and typeIDs with the use
    of the RegisterMessageType function. The unmarshalling process uses
    reflection and the type stored in the type map to create a payload
    instance of the appropriate type.

--- common/dtos/websocket_message.go
+++ common/dtos/websocket_message.go
@@ -2,7 +2,10 @@ package dtos
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
+	"reflect"
+	"strconv"
 )
 
 //WebSocketMessageType represents the type of the message.
@@ -27,7 +30,7 @@ const (
 type WebSocketMessage struct {
 	MessageType WebSocketMessageType `json:"messageType"`
 	RequestID   int64                `json:"requestID"`
-	Payload     *json.RawMessage     `json:"payload"`
+	Payload     interface{}          `json:"payload"`
 }
 
 //WebSocketMessageMarshaller allows conversion from byte slices to WSMessage structs
@@ -92,4 +95,64 @@ func NewErrorMsg(subsystem string, err error, requestID int64) WebSocketMessage 
 		Payload:     (*json.RawMessage)(&buf),
 		RequestID:   requestID,
 	}
+}
+
+var typeMap = make(map[WebSocketMessageType]reflect.Type)
+var reversedTypeMap = make(map[reflect.Type]WebSocketMessageType)
+
+func init() {
+	RegisterMessageType(WSMsgAuthenticationRequest, AuthenticationRequest{})
+	RegisterMessageType(WSMsgAuthenticationResponse, AuthenticationResponse{})
+	RegisterMessageType(WSMsgError, Error{})
+}
+
+/*RegisterMessageType registers the type for both marshalling and unmarshalling.
+This function is NOT thread-safe and should be preferably called in the init()
+function of a higher-level package.*/
+func RegisterMessageType(typeID WebSocketMessageType, payload interface{}) {
+	_, found := typeMap[typeID]
+	if found {
+		log.Panicf("Type(ID: %d, %v) already registered!  ", typeID, payload)
+	}
+	t := reflect.ValueOf(payload).Type()
+	typeMap[typeID] = t
+	reversedTypeMap[t] = typeID
+}
+
+/*NewWebSocketMessage constructs a WebSocketMessage and sets the appropriate
+messageType. If the payload type is not in the typemap the function will panic.*/
+func NewWebSocketMessage(requestID int64, p interface{}) WebSocketMessage {
+	v := reflect.ValueOf(p)
+	msgType, found := reversedTypeMap[v.Type()]
+	if !found {
+		log.Panicf("Unknown payload type (payload:%s)", v.Type().String())
+	}
+	return WebSocketMessage{
+		MessageType: msgType,
+		RequestID:   requestID,
+		Payload:     p,
+	}
+}
+
+/*UnmarshalJSON unmarshals the json string into a websocket message. If the
+message type is unknown or the string is malformed an error is returned.*/
+func (w *WebSocketMessage) UnmarshalJSON(data []byte) error {
+	temp := struct {
+		MessageType WebSocketMessageType `json:"messageType"`
+		RequestID   int64                `json:"requestID"`
+		PayloadData *json.RawMessage     `json:"payload"`
+	}{}
+	err := json.Unmarshal(data, &temp)
+	if err != nil {
+		return err
+	}
+
+	w.RequestID = temp.RequestID
+	w.MessageType = temp.MessageType
+	payloadType, found := typeMap[temp.MessageType]
+	if !found {
+		return errors.New("Unknown message type: " + strconv.Itoa(int(w.MessageType)))
+	}
+	w.Payload = reflect.New(payloadType).Interface()
+	return json.Unmarshal([]byte(*temp.PayloadData), w.Payload)
 }
