5a002263b93eb8829302b3d3065944fea19e5b31
Damian Jarek <damian.jarek93@gmail.com>
2016-03-25 22:53:35 +0100

    Fix master-control parser
    The parser will now properly use the new serialization infrastructure.

--- master/parser.go
+++ master/parser.go
@@ -41,24 +41,23 @@ func (mp messageParser) ParseRecvMsg(msg dtos.WebSocketMessage, connection *wspr
 var auth = authenticator{}
 
 func onAuthenticationRequest(msg dtos.WebSocketMessage, connection *wsprotocol.Connection) {
-	var credentials dtos.AuthenticationRequest
-	err := json.Unmarshal(*msg.Payload, &credentials)
-	if err != nil {
-		log.Println("Error when unmarshalling credentials: " + err.Error())
+	credentials, ok := msg.Payload.(*dtos.AuthenticationRequest)
+	if !ok {
+		log.Printf("Invalid payload type (typeID:%d, payload:%v)\n", msg.MessageType,
+			msg.Payload)
 		return
 	}
+
 	response := dtos.AuthenticationResponse{
 		Result: "auth_wrong",
 	}
-
-	authErr := auth.Authenticate(credentials)
+	authErr := auth.Authenticate(*credentials)
 	if authErr == nil {
 		response.Result = "auth_ok"
 	}
-	marshalPayload(&msg.Payload, response)
-	msg.MessageType = dtos.WSMsgAuthenticationResponse
 
-	channel, err := connection.SendAsync(msg)
+	responseMsg := dtos.NewWebSocketMessage(msg.RequestID, response)
+	channel, err := connection.SendAsync(responseMsg)
 	if err != nil && authErr == nil {
 		go func() {
 			err := <-channel
